acaa5a958f12535bdb0c248ac2fe36f376f825a1
Michael <michael.c.montgomery@gmail.com>
2022-11-11 16:29:38 -0600

    added login component and routes

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,12 +1,22 @@
 import React, {useState} from 'react'
+import {Routes, Route} from 'react-router-dom'
 import SignUp from './SignUp';
+import Login from './Login';
 
 function App() {
   const {login, setlogin} = useState("")
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [passwordConfirmation, setPasswordConfirmation] = useState("")
 
   return (
     <div className="App">
-      <SignUp login={login}/>
+      <Routes>
+        {/* <Route exact path="/" element={}/> */}
+        <Route exact path="/signup" element={<SignUp username={username} setUsername={setUsername} email={email} setEmail={setEmail} password={password} setPassword={setPassword} passwordConfirmation={passwordConfirmation} setPasswordConfirmation={setPasswordConfirmation} login={login}/>}/>
+        <Route exact path="/login/" element={<Login username={username} setUsername={setUsername} password={password} setPassword={setPassword} passwordConfirmation={passwordConfirmation} setPasswordConfirmation={setPasswordConfirmation} login={login}/>} />
+      </Routes>
     </div>
   );
 }
--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,10 +1,7 @@
-import React, {useState} from 'react'
+import React from 'react'
 import {useNavigate} from "react-router-dom"
 
-function SignUp({login}) {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
-    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+function SignUp({username, setUsername, email, setEmail, password, setPassword, passwordConfirmation, setPasswordConfirmation, login}) {
     const navigate = useNavigate()
   
     function handleSubmit(e) {
@@ -32,26 +29,13 @@ function SignUp({login}) {
       <div>
         <form onSubmit={handleSubmit}>
           <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
+          <input type="text" id="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+          <label htmlFor='email'>Email:</label>
+          <input type="text" id="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
           <label htmlFor="password">Password:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
           <label htmlFor="password_confirmation">Confirm Password:</label>
-          <input
-            type="password"
-            id="password_confirmation"
-            value={passwordConfirmation}
-            onChange={(e) => setPasswordConfirmation(e.target.value)}
-          />
+          <input type="password" id="password_confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)}/>
           <button type="submit">Submit</button>
         </form>
         <h3>Already have an account?</h3>
