a1d7ef584833d230c3f34b94085a0427be6d8cf6
Michael <michael.c.montgomery@gmail.com>
2022-11-11 14:04:26 -0600

    reverted some changes

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,5 +1,4 @@
 import React, {useState} from 'react'
-import './App.css';
 import SignUp from './SignUp';
 
 function App() {
--- client/src/Components/SignUp.js
+++ client/src/Components/SignUp.js
@@ -1,9 +1,11 @@
 import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
 
 function SignUp({login}) {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const navigate = useNavigate()
   
     function handleSubmit(e) {
       e.preventDefault();
@@ -21,32 +23,40 @@ function SignUp({login}) {
         .then((r) => r.json())
         .then(login);
     }
+
+    function navigateToLoginPage() {
+      navigate('/login')
+    }
   
     return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username:</label>
-        <input
-          type="text"
-          id="username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <label htmlFor="password">Password:</label>
-        <input
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <label htmlFor="password_confirmation">Confirm Password:</label>
-        <input
-          type="password"
-          id="password_confirmation"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <label htmlFor="password_confirmation">Confirm Password:</label>
+          <input
+            type="password"
+            id="password_confirmation"
+            value={passwordConfirmation}
+            onChange={(e) => setPasswordConfirmation(e.target.value)}
+          />
+          <button type="submit">Submit</button>
+        </form>
+        <h3>Already have an account?</h3>
+        <button onClick={navigateToLoginPage}>Login</button>
+      </div>
     );
 }
 
