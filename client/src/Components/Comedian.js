dd4a95ab3aefa8c526fe891a1314a8e295dd9bf7
Michael <michael.c.montgomery@gmail.com>
2022-11-12 10:33:12 -0600

    set up Comedian component to read data

--- client/src/Components/Comedian.js
+++ client/src/Components/Comedian.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Comedian({comedians}) {
+function Comedian({singleComedian}) {
 
     return (
-        <div>
-            
+        <div className='comedian'>
+            <h3>{singleComedian.name}</h3>
+            <p>{singleComedian.average_rating}</p>
+            <p>{singleComedian.review_count}</p>
+            <p>{singleComedian.bio}</p>
         </div>
     )
 }
