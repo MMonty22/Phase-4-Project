7102be16bc33fd8cdfa363c91f322de98e19bb55
Michael <michael.c.montgomery@gmail.com>
2022-11-12 10:58:00 -0600

    started navigate to review form process

--- client/src/Components/Comedian.js
+++ client/src/Components/Comedian.js
@@ -1,6 +1,15 @@
 import React from 'react'
+import {useNavigate, useSearchParams} from "react-router-dom"
 
 function Comedian({singleComedian}) {
+    const navigate = useNavigate()
+    const [searchParams] = useSearchParams()
+    const comedianID = searchParams.get("comedian_id")
+    const userID = searchParams.get("user_id")
+
+    function navigateToReviewForm() {
+        navigate(`/review/${userID}/${comedianID}`)
+    }
 
     return (
         <div className='comedian'>
@@ -8,6 +17,7 @@ function Comedian({singleComedian}) {
             <p>{singleComedian.average_rating}</p>
             <p>{singleComedian.review_count}</p>
             <p>{singleComedian.bio}</p>
+            <button onClick={navigateToReviewForm}>Leave a Review</button>
         </div>
     )
 }
