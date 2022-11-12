b2001d15de72cbaade5eb62aff8b37302d8c2858
Michael <michael.c.montgomery@gmail.com>
2022-11-12 10:59:06 -0600

    created review form component

---
+++ client/src/Components/ReviewForm.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ReviewForm() {
+
+    function handleSubmit() {
+
+    }
+
+    return(
+        <div>
+            <form onSubmit={handleSubmit}>
+
+            </form>
+        </div>
+    )
+}
+
+export default ReviewForm
