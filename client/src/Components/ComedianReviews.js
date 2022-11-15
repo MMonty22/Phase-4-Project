db7c44cc5112409009368efd7c836c052ffaca55
Michael <michael.c.montgomery@gmail.com>
2022-11-15 17:23:31 -0600

    added more to comedian reviews component

--- client/src/Components/ComedianReviews.js
+++ client/src/Components/ComedianReviews.js
@@ -2,15 +2,19 @@ import React from "react";
 import {useSearchParams} from "react-router-dom"
 
 function ComedianReviews({comedians}) {
-    const comedian = comedians.map((singleComedian) => console.log(singleComedian))
     const [searchParams] = useSearchParams()
-    const comedianID = searchParams.get("comedian_id")
+    const comedianID = searchParams.get("id")
     const relevantComedian = comedians.filter((comedian) => String(comedian.id) === String(comedianID))
+    //console.log('RelevantComedian', relevantComedian)
 
     return (
         <div>
-            <h1>{relevantComedian.name}'s Reviews</h1>
-            <p>{relevantComedian.reviews[0].review_text}</p>
+            <h1>{relevantComedian[0].name}'s Reviews</h1>
+            {relevantComedian[0]?.reviews?.map(review => (<ul key={review.id}>
+                <li>User: {}</li>
+                <li>Rating: {review.rating}</li>
+                <li>{review.review_text}</li>
+            </ul>))}
         </div>
     )
 }
