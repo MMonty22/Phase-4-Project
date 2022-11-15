5c5e5fc371c5b4c135544e108afd661921a950f1
Michael <michael.c.montgomery@gmail.com>
2022-11-15 16:53:06 -0600

    added ot comedian review component

--- client/src/Components/ComedianReviews.js
+++ client/src/Components/ComedianReviews.js
@@ -1,9 +1,16 @@
 import React from "react";
+import {useSearchParams} from "react-router-dom"
 
 function ComedianReviews({comedians}) {
+    const comedian = comedians.map((singleComedian) => console.log(singleComedian))
+    const [searchParams] = useSearchParams()
+    const comedianID = searchParams.get("comedian_id")
+    const relevantComedian = comedians.filter((comedian) => String(comedian.id) === String(comedianID))
+
     return (
         <div>
-
+            <h1>{relevantComedian.name}'s Reviews</h1>
+            <p>{relevantComedian.reviews[0].review_text}</p>
         </div>
     )
 }
