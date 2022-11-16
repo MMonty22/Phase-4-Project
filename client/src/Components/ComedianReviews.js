c0160a082321c0484db3a3714f7ad9926666a3f5
Michael <michael.c.montgomery@gmail.com>
2022-11-16 16:32:11 -0600

    added username to comedian reviews component

--- client/src/Components/ComedianReviews.js
+++ client/src/Components/ComedianReviews.js
@@ -6,12 +6,14 @@ function ComedianReviews({comedians}) {
     const comedianID = searchParams.get("id")
     const relevantComedian = comedians.filter((comedian) => String(comedian.id) === String(comedianID))
     //console.log('RelevantComedian', relevantComedian)
+    const relevantUser = relevantComedian[0].users.filter((user) => String(user.id) === String(relevantComedian[0].reviews.map((review)=> review.user_id)))
+    //console.log('RelevantUser', relevantUser)
 
     return (
         <div>
             <h1>{relevantComedian[0].name}'s Reviews</h1>
             {relevantComedian[0]?.reviews?.map(review => (<ul key={review.id}>
-                <li>User: {}</li>
+                <li>User: {relevantUser[0].username}</li>
                 <li>Rating: {review.rating}</li>
                 <li>{review.review_text}</li>
             </ul>))}
