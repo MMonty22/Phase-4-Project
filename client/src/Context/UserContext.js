63620003114a7e4719e6c43b9993cf610a2c7c4d
Michael <michael.c.montgomery@gmail.com>
2022-11-16 18:04:00 -0600

    added to user context

--- client/src/Context/UserContext.js
+++ client/src/Context/UserContext.js
@@ -2,11 +2,38 @@ import React, {useState, useEffect, createContext} from "react";
 
 const userContext = createContext()
 
-function UserProvider() {
-    return (
-        <div>
+function UserProvider({children}) {
+    const [user, setUser] = useState({})
+    const [loggedIn, setLoggedIn] = useState(false)
 
-        </div>
+    useEffect(() => {
+        fetch('/user')
+        .then(res => res.json())
+        .then(data => {
+            setUser(data)
+            data.error ? setLoggedIn(false) : setLoggedIn(true)
+        })
+    })
+
+    function login(user) {
+        setUser(user)
+        setLoggedIn(true)
+    }
+
+    function logout() {
+        setUser({})
+        setLoggedIn(false)
+    }
+
+    function signup(user) {
+        setUser(user)
+        setLoggedIn(true)
+    }
+
+    return (
+        <userContext.Provider value={{user, login, logout, signup, loggedIn}}>
+            {children}
+        </userContext.Provider>
     )
 }
 
