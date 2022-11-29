b7218022beeffed4643f8f0d5a127b5c58cfe915
Michael <michael.c.montgomery@gmail.com>
2022-11-29 16:43:06 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :comedians
-  resources :reviews
-  resources :users
+  resources :comedians, only: [:index, :create]
+  resources :reviews, only: [:index, :create, :update, :destroy]
+  resources :users, only: [:index, :create, :show]
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
